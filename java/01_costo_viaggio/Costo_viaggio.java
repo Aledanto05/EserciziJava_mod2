f07bff480282f97a252fb154b04a51f4595221da
Andrea Calabretta <andreacalabretta0@gmail.com>
2024-03-10 17:17:40 +0100

    fix(01): remove useless code

--- java/01_costo_viaggio/Costo_viaggio.java
+++ java/01_costo_viaggio/Costo_viaggio.java
@@ -4,8 +4,6 @@
 import java.util.Scanner;
 public class Costo_viaggio {
         
-    public Costo_viaggio() {
-    }
     public static void main(String[] args) {
          Scanner scanner = new Scanner(System.in);
         
