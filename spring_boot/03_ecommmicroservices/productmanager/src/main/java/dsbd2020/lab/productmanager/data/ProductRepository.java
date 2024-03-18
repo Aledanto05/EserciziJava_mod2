af17f23c8adffdc10c7249491eef9c3fe39fe398
Andrea Calabretta <andreacalabretta0@gmail.com>
2024-03-18 09:59:30 +0100

    feat(03_ecom_docker): remove

--- spring_boot/03_ecommmicroservices/productmanager/src/main/java/dsbd2020/lab/productmanager/data/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package dsbd2020.lab.productmanager.data;
-
-import org.springframework.data.repository.CrudRepository;
-import product.Product;
-
-public interface ProductRepository extends CrudRepository<Product, Integer> {
-    public Product findByIdAndQuantityGreaterThanEqual(Integer id, Integer quantity);
-
-}
--- spring_boot/03_ecommmicroservices/usermanager/src/main/java/dsbd2020/lab/usermanager/data/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package dsbd2020.lab.usermanager.data;
-
-import org.springframework.data.repository.CrudRepository;
-import user.User;
-
-public interface UserRepository extends CrudRepository<User,Integer> {
-    public User findByEmail(String email);
-
-}
