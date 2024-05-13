89d1ec5ca832b093dd28540071c5edcc93e8d096
Andrea Calabretta <andreacalabretta0@gmail.com>
2024-05-13 12:19:15 +0200

    feat(math): @controller add

--- spring_boot/02_math/src/main/java/org/code/math/controller/MathController.java
+++ spring_boot/02_math/src/main/java/org/code/math/controller/MathController.java
@@ -6,10 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-
+@Controller
 @RequestMapping(path = "math")
 public class MathController {
-
     @Autowired
     OperationRepository operationRepository;
 
